fa03b829b27f1fd92a667c87d1d420be66c69dd7
Akin Karayun <akinkarayun1@hotmail.com>
2022-05-10 21:32:46 +0300

    created the search bar and added the auto complete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Trie from './utils/Trie';
+
+const dictionary = {
+  words: ['hello linked in', 'helium', 'world', 'car', 'carpet', 'test', 'this', 'that', 'those', 'working', 'is', 'this',]
+}
 
 function App() {
+
+  const [prefix, setPrefix] = React.useState("");
+  const [suggestion, setSuggestion] = React.useState("");
+
+  const getWords = async () => {
+    const url = 'https://raw.githubusercontent.com/EKaxada/webster-words/main/dict.json'
+    const res = await fetch(url, {
+      method: 'GET'
+    });
+    console.log(res)
+    return await res.json();
+  }
+
+  getWords()
+
+
+
+  var myTrie = new Trie();
+
+
+  (async () => {
+    // const dictionary = await getWords();
+    const words = dictionary.words;
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      myTrie.insert(word)
+    }
+  })();
+
+  const onChange = (e: any) => {
+    var value = e.target.value;
+    setPrefix(value);
+    var words = value.split(" ");
+    var trie_prefix = words[words.length - 1].toLowerCase();
+    var found_words = myTrie.find(trie_prefix).sort((a, b) => {
+      return a.length - b.length;
+    });
+    var first_word = found_words[0];
+    if (
+      found_words.length !== 0 &&
+      value !== "" &&
+      value[value.length - 1] !== " "
+    ) {
+      if (first_word != null) {
+        var remainder = first_word.slice(trie_prefix.length);
+        setSuggestion(value + remainder);
+      }
+    } else {
+      setSuggestion(value);
+    }
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (e.keyCode === 39) {
+      setPrefix(suggestion);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        type="text"
+        name="search-bar"
+        id="search-bar"
+        placeholder="Search..."
+        value={prefix}
+        onChange={onChange}
+        onKeyDown={handleKeyDown}
+      />
+      <input
+        type="text"
+        name="search-bar"
+        id="search-bar2"
+        value={suggestion}
+      />
     </div>
   );
 }
